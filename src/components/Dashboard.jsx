2f506f7f50bd097b6724687b2139b6edc9bd6a65
Pablo Sarasua <yo.sarasua@gmail.com>
2025-06-19 15:55:53 -0300

    se borra imports innecesarios

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,12 +1,4 @@
-import React, { useEffect, useState } from "react";
-import { Pie } from "react-chartjs-2";
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
-import  supabase  from "../supabaseClient";
-
-ChartJS.register(ArcElement, Tooltip, Legend);
-
 export default function Dashboard() {
-
   return(
     <div className="container-fluid mt-4">
       <h3>Sin información</h3>
