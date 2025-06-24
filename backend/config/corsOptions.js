d33671abd0f3c2e203688de0ca46a3d71e25ff4a
Pablo Sarasua <yo.sarasua@gmail.com>
2025-06-24 16:38:30 -0300

    agregado cors de produccion

--- backend/config/corsOptions.js
+++ backend/config/corsOptions.js
@@ -1,7 +1,8 @@
 // config/corsOptions.js
 // Configuración de CORS para restringir orígenes permitidos y mejorar la seguridad.
 const allowedOrigins = [
-  'http://localhost:5173', // Cambia esto por el dominio de tu frontend en producción
+  'http://localhost:5173', // para desarrollo local
+  'https://mapas-clientes-front.onrender.com', // tu frontend en producción
 ];
 
 export const corsOptions = {
